2dc206034b8c79ca828afe53ff074af66363eb70
Andrew Gorman <andrewgorman101@gmail.com>
2020-11-09 18:29:06 +0000

    update 0auth redirect url to heroku domain

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 ReactDOM.render(
-    <Auth0Provider domain={domain} clientId={clientId} redirectUri={'http://localhost:3000/'}>
+    <Auth0Provider domain={domain} clientId={clientId} redirectUri={'https://alamo-d19124355.herokuapp.com/'}>
         <App />
     </Auth0Provider>,
     document.getElementById('root')
