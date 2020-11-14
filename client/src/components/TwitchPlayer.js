e0fff52449c09e8839863155beed0457de2717c0
Andrew Gorman <andrewgorman101@gmail.com>
2020-11-14 00:54:03 +0000

    update twitch player url

--- client/src/components/TwitchPlayer.js
+++ client/src/components/TwitchPlayer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TwitchPlayer = (props) => {
-    const url = `https://player.twitch.tv/?channel=${props.twitchChannel}&migration=true&parent=alamo-d19124355.herokuapp.com/`
+    const url = `https://player.twitch.tv/?channel=${props.twitchChannel}&migration=true&parent=alamo-d19124355.herokuapp.com`
 
     return (
         <div className="row">
