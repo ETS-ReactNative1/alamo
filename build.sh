94b860297eb23d47a7c0bdbd4da9ef70d52ebfe7
Andrew <andrewgorman101@gmail.com>
2020-12-06 19:59:18 +0000

    bash running in development mode only

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 echo 'Starting Alamo build.'
-npm install --save
 chmod +x ./nodejs.sh
-open -a Terminal.app nodejs.sh 
+open -b com.apple.terminal ./nodejs.sh
 chmod +x ./peerjs.sh
-open -a Terminal.app peerjs.sh 
+open -b com.apple.terminal ./peerjs.sh
 echo 'Starting ReactJS'
-cd client && npm install --save && npm run build
-npm install --save serve 
-npm serve -s build
+cd client && npm install --save && npm run dev 
 echo 'All Good. Enjoy!'
