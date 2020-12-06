b06967531368c6d8a232a43662a06e91a3af6b1e
Andrew <andrewgorman101@gmail.com>
2020-12-06 18:55:24 +0000

    nodejs script added

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 echo 'Starting Alamo build.'
 npm install --save
-echo 'Starting ReactJS'
-cd client && npm install --save && npm run build
+chmod +x ./nodejs.sh
+open -a Terminal.app nodejs.sh 
 chmod +x ./peerjs.sh
 open -a Terminal.app peerjs.sh 
+echo 'Starting ReactJS'
+cd client && npm install --save && npm run build
+npm install --save serve 
+npm serve -s build
 echo 'All Good. Enjoy!'
