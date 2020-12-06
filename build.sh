963847e56c98db9312b3cf52acd55e38ee62629c
Andrew <andrewgorman101@gmail.com>
2020-12-06 20:06:18 +0000

    update bash script

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 echo 'Starting Alamo build.'
+npm install
 chmod +x ./nodejs.sh
 open -b com.apple.terminal ./nodejs.sh
 chmod +x ./peerjs.sh
 open -b com.apple.terminal ./peerjs.sh
 echo 'Starting ReactJS'
-cd client && npm install --save && npm run dev 
+cd client
+npm install
+npm run dev
 echo 'All Good. Enjoy!'
